2a8bee7faa931be82fc03e8e939a72b173cd5312
wuzd <429023964@qq.com>
2019-09-09 18:16:25 +0800

    项目结构配置

--- lib/search.dart
+++ lib/search.dart
@@ -31,31 +31,100 @@ class SearchState extends State<Search> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-        body: new Container(
-      margin: const EdgeInsets.only(top: 25.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Stack(
-            children: <Widget>[
-              TouchCallBack(
-                isfeed: false,
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Container(
-                  height: 45.0,
-                  margin: EdgeInsets.only(left: 12.0, right: 12.0),
-                  child: Icon(
-                    Icons.chevron_left,
-                    color: Colors.black,
+      body: new Container(
+        margin: const EdgeInsets.only(top: 25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                TouchCallBack(
+                  isfeed: false,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    height: 45.0,
+                    margin: EdgeInsets.only(left: 12.0, right: 12.0),
+                    child: Icon(
+                      Icons.chevron_left,
+                      color: Colors.black,
+                    ),
                   ),
                 ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  height: 45.0,
+                  margin: const EdgeInsets.only(left: 50.0, right: 10.0),
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(width: 1.0, color: Colors.green),
+                    ),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(
+                        child: TextField(
+                          focusNode: _requestFocus(),
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16.0,
+                          ),
+                          onChanged: (String text) {},
+                          decoration: InputDecoration(
+                            hintText: '搜索',
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(right: 10.0),
+                        child: Icon(
+                          Icons.mic,
+                          color: Color(0xffaaaaaa),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 50.0),
+              child: Text(
+                '常用搜索',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Color(0xffb5b5b5),
+                ),
               ),
-            ],
-          ),
-        ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  _getText('朋友'),
+                  _getText('聊天'),
+                  _getText('群组'),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  _getText('Flutter'),
+                  _getText('Dart'),
+                  _getText('C++'),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
